5f6da069864723e40870dba16ad0a5ff8ff59bf6
Shubham Mittal <shubhammittal2001@zohomail.in>
2026-06-22 16:30:42 +0530

    feat: proxy base-convertor app at /project/base-convertor
    Signed-off-by: Shubham Mittal <shubhammittal2001@zohomail.in>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/project/base-convertor',
+        destination: 'https://base-convertor-one.vercel.app',
+      },
+      {
+        source: '/project/base-convertor/:path*',
+        destination: 'https://base-convertor-one.vercel.app/:path*',
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
